f43dc001d0fcd04b49d967cd0e7ddef13e187d21
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2023-11-04 23:25:13 +0530

    Basic frontend + gpt-unsplash api

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["lh3.googleusercontent.com", ],
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      typescript: {
+        ignoreBuildErrors: true,
+      },
+      output: "standalone",
+    };
+
+module.exports = nextConfig
