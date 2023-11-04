0b6969cb6177e31bf971c0d2beebc768babf48ab
Hatif <md.hatifosmani15@gmail.com>
2023-11-05 05:13:57 +0530

    youtube + chapter creation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter, Lexend } from 'next/font/google'
-import './globals.css'
-import { cn } from '@/lib/utils'
-import Navbar from '@/components/Navbar'
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Lexend } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import { Provider } from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
 
-const lexend = Lexend({ subsets: ['latin'] })
+const lexend = Lexend({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Learning Journey',
-}
+  title: "Learning Journey",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={cn(lexend.className, " antialiased min-h-screen pt-16")}>
-        <Navbar/>
-        {children}
+       <body className={cn(lexend.className, "antialiased min-h-screen pt-16")}>
+        <Provider>
+          <Navbar />
+          {children}
+          <Toaster />
+        </Provider>
       </body>
     </html>
-  )
+  );
 }
