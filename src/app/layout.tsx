3413428b91f97c06f7d84ea280e259e4476f6092
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2023-11-05 07:00:48 +0530

    Quiz section and UI for courses added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
-import { cn } from "@/lib/utils";
-import "./globals.css";
 import type { Metadata } from "next";
-import { Lexend } from "next/font/google";
+import { Inter, Lexend } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
-import { Provider } from "@/components/Providers";
-import { Toaster } from "@/components/ui/toaster";
 
 const lexend = Lexend({ subsets: ["latin"] });
 
@@ -19,12 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-       <body className={cn(lexend.className, "antialiased min-h-screen pt-16")}>
-        <Provider>
-          <Navbar />
-          {children}
-          <Toaster />
-        </Provider>
+      <body className={cn(lexend.className, " antialiased min-h-screen pt-16")}>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
